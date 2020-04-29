65df6ec6258dea07ce8087c9abe619a0a2532767
lexow1337 <lexow1337@gmail.com>
2020-04-29 19:35:49 +0200

    Klassen für Shopverwaltung Implementiert

--- src/main/java/eshop/valueobjects/Kunde.java
+++ src/main/java/eshop/valueobjects/Kunde.java
@@ -1,4 +1,55 @@
 package eshop.valueobjects;
 
-public class Kunde {
+public class Kunde extends Nutzer {
+
+    private String strasse;
+    private String hausnr = "";
+    private String plz = "";
+    private String ort = "";
+
+    public Kunde(String vorname, String nachname, String login, String passwort, int nummer, boolean isMitarbeiter) {
+        super(vorname, nachname, login, passwort, nummer, isMitarbeiter);
+    }
+
+    public Kunde(String vorname, String nachname, String login, String passwort, int nummer, boolean isMitarbeiter, String strasse, String hausnr, String plz, String ort) {
+        super(vorname, nachname, login, passwort, nummer, isMitarbeiter);
+        this.strasse = strasse;
+        this.hausnr = hausnr;
+        this.plz = plz;
+        this.ort = ort;
+    }
+
+    // Getter
+    public String getStrasse() {
+        return strasse;
+    }
+
+    public String getHausnr() {
+        return hausnr;
+    }
+
+    public String getPlz() {
+        return plz;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    //Setter
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+
+    public void setHausnr(String hausnr) {
+        this.hausnr = hausnr;
+    }
+
+    public void setPlz(String plz) {
+        this.plz = plz;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
 }
